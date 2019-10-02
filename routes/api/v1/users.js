877627f7247b33f016c3f77f80950a0781d26966
Jori Peterson <joripeterson@gmail.com>
2019-10-01 21:04:50 -0600

    Create successful post request (minus api key full generation) for creating users

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,9 +3,28 @@ var router = express.Router();
 
 var user = require('../../../models').User
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+function generateAPI(){
+  return "1234";
+}
+
+router.post("/", function(req, res, next){
+  res.setHeader("Content-type", "application/json")
+
+  user.create({
+    email: req.body.email,
+    password: req.body.password,
+    apiKey: generateAPI()
+  })
+    .then(user => {
+      payload = {
+        apiKey: generateAPI()
+      }
+      res.status(201).send(payload)
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({error})
+    })
+})
 
 module.exports = router;
