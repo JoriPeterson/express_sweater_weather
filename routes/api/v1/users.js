522176e8ab61e3c7b446f6b96ef69e944fa3e342
Jori Peterson <joripeterson@gmail.com>
2019-10-02 20:21:21 -0600

    Add sad paths for user endpoints

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -8,7 +8,8 @@ const uuidv1 = require('uuid/v1');
 router.post("/", function(req, res, next){
   res.setHeader("Content-type", "application/json")
 
-  if (req.body.password === req.body.passwordConfirmation){
+  if (req.body.password === req.body.passwordConfirmation
+    && req.body.password.length > 1){
     user.create({
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password, saltRounds),
@@ -17,13 +18,13 @@ router.post("/", function(req, res, next){
     .then(user => {
       res.status(201).send({ api_key: user.apiKey })
     })
+    .catch(error => {
+      res.status(500).send("Oh no! Something went wrong!")
+    })
 
   } else {
       res.status(401).send("Your information is not correct!");
     }
-    .catch(error => {
-      res.status(500).send("Oh no! Something went wrong!")
-    })
   })
 
 module.exports = router;
