b6209a0f41f07233c510900e409669833f239860
Jori Peterson <joripeterson@gmail.com>
2019-10-03 21:02:11 -0600

    Update google api call structure

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,13 +1,15 @@
 var express = require('express');
-var fetch = require('node-fetch')
 var router = express.Router();
+var fetch = require('node-fetch')
 var dotenv = require('dotenv').config()
 var location = require('../../../models').Location
 
-function getCoordinates() {
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?${location}&key=${process.env.GOOGLE_API_KEY}`)
+router.get("/", function(req, res, next){
+  res.setHeader("Content-type", "application/json")
+
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.location}&key=${process.env.GOOGLE_API_KEY}`)
   .then(response => response.json())
   .then(result => console.log(result))
-}
+})
 
 module.exports = router;
