9d4e1f1048f66eb47fa4c7efad0387bf7ce0c767
Jori Peterson <joripeterson@gmail.com>
2019-10-06 10:57:22 -0600

    Get geocoding api call to work

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -1,15 +1,27 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router()
 var fetch = require('node-fetch')
 var dotenv = require('dotenv').config()
 var location = require('../../../models').Location
 
 router.get("/", function(req, res, next){
+  let location;
   res.setHeader("Content-type", "application/json")
-
-  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.location}&key=${process.env.GOOGLE_API_KEY}`)
+  // findOne where req.body.api_key exists
+  // findOne (location)
+  // no location
+  // then
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GOOGLE_API_KEY}`)
   .then(response => response.json())
-  .then(result => console.log(result))
+  .then(result => location = {location: req.query.location,
+                              lat: result['results'][0]['geometry']['location']['lat'],
+                              lng: result['results'][0]['geometry']['location']['lng'])}
+  Location.create(location)
+  // fetch(DarkSkyAPI (location.lat, location.lng))
+
+  // send parsed json - user story requirements
+
 })
 
+// async await... instead of .then if you don't store in database
 module.exports = router;
