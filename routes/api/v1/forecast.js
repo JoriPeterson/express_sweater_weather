63d4094ac8bfcc68604b2714bfcaf19b0b00cf63
Jori Peterson <joripeterson@gmail.com>
2019-10-06 21:15:05 -0600

    Attempt to get forecast endpoint; Not getting nested fetch call back

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -9,53 +9,52 @@ router.get("/", function(req, res, next){
   console.log("hit user")
   res.setHeader("Content-type", "application/json")
 
-  User.findOne({
-    where: {
-      apiKey: req.body.api_key
+  // User.findOne({
+  //   where: {
+  //     apiKey: req.body.api_key
+  //   }
+  // .then(user => {
+
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GOOGLE_API_KEY}`)
+  .then( response => {
+    return response.json()
+  })
+  .then( result => {
+    console.log(result)
+    return result
+    // res.status(207).send(JSON.stringify(result));
+    // res.status(207).send(result)
+    // return res
+  })
+  .then(geocode => {
+    var location = {
+      location: req.query.location,
+      lat: geocode['results'][0]['geometry']['location']['lat'],
+      lng: geocode['results'][0]['geometry']['location']['lng']
     }
+    Location.create(location)
+    return location
+    // res.status(207).send(JSON.stringify(location));
   })
-  .then(user => {
-    console.log(req.query.location)
-    // return location
-    // Location.find ({
-    //   where: {
-    //     location: req.query.location
-    //   }
-    // })
-    // .then(location => {
-      // console.log(location)
-      // if (location === null) {
-        fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GOOGLE_API_KEY}`)
-        .then(response => response.json())
-        .then(result => function()
-          { var location = {location: req.query.location,
-          lat: result['results'][0]['geometry']['location']['lat'],
-          lng: result['results'][0]['geometry']['location']['lng']}
+  .then( location => {
+      var ds_url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${location.lat},${location.lng}?exclude=minutely`
+      console.log(ds_url)
 
-          Location.create(location)
-          console.log(location)
-          return(location)})
-
-        // .catch(error => {
-        //   res.status(401).send({what: "Some location problem"})
-        // })
-      // }
-    // })
-    .then(location => {
-      console.log(`https://api.darksky.net/forecast/${process.env.DarkSkyAPI}/${location.lat},${location.lng}?exclude=minutely`)
-
-      fetch(`https://api.darksky.net/forecast/${process.env.DarkSkyAPI}/${location.lat},${location.lng}?exclude=minutely`)
-      .then(response => response.json())
-      .then(res.status(200).send(result['currently']))
-
-    })
-    .catch(error => {
-      res.status(401).send({what: "Some other problem"})
-    })
+      return fetch(ds_url, {headers: {Accept: 'application/json'}})
   })
-  // .catch(error => {
-  //   res.status(401).send({what: "Invalid API Key"})
-  // })
+  .then( response => {
+    return response.json();
+  })
+  .then(result => {
+    console.log(result)
+    res.status(207).send(result);
+  })
+  .catch( err => {
+      res.status(411).send("fail");
+  })
 })
-
 module.exports = router;
+
+    // fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.location}&key=${process.env.GOOGLE_API_KEY}`)
+
+    // fetch(`https://api.darksky.net/forecast/${process.env.DarkSkyAPI}/${location.lat},${location.lng}?exclude=minutely`)
